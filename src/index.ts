ad533b960e622fc32ca52b2f27a4199f3fea3053
majed1458 <majedbenkhalifa99@gmail.com>
2024-01-24 17:32:22 +0100

    fixing sockets

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const http = require('http'); // to use HTTPS, use: require('https') and the "op
 // const https = require('spdy'); // for HTTP2
 const fs = require('fs');
 const app = require('./config/express');
-const socket = require('./api/services/socket');
+
 
 const mongoose = require('./config/mongoose');
 
@@ -17,14 +17,105 @@ mongoose.connect(); // open mongoose connection
 // HTTPS options
 const options = {};
 // const options = {
-//   key: fs.readFileSync(__dirname + '/config/https/localhost-key.pem'),
-//   cert: fs.readFileSync(__dirname + '/config/https/localhost.pem')
-// };
-const server = http.createServer(options, app);
+  //   key: fs.readFileSync(__dirname + '/config/https/localhost-key.pem'),
+  //   cert: fs.readFileSync(__dirname + '/config/https/localhost.pem')
+  // };
+  const server = http.createServer(options, app);
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      
+    },
+    });
 
-if (socketEnabled) {
-  socket.setup(server);
-}
+
+let onlineUser:Array<any> = []
+io.on('connection', (socket: any) => {
+
+
+  socket.on('disconnect', () => {
+    socket.leave(socket.id)  
+    onlineUser.map(x=>{
+      console.log(x);
+      
+      x.userSocket.map((y:any,index:any) =>{
+        console.log(index);
+        
+        if(y === socket.id)
+        x.userSocket.splice(index, 1);
+
+      })
+    })
+    console.log(onlineUser);
+    
+    socket.emit('onlineuser',onlineUser)
+  });        
+
+        socket.on('newUser',(data:any) =>{ 
+            
+            if(data){
+              let userId:string =data;
+             
+              let userExsit = onlineUser.find((x:any)=> x.userId == userId )
+              
+              if(!userExsit){
+                let arrayTab = [];
+                arrayTab.push(socket.id)
+                console.log(userId);0
+                onlineUser.push({userId,userSocket:arrayTab})
+              }else{
+                userExsit.userSocket.push(socket.id)
+              }
+            }
+            
+            io.emit('onlineuser',onlineUser)
+        })
+        
+  
+
+  socket.on('join', (data: any) => {
+    
+    let user = data.user;
+    let conversation = data.conversation
+    
+    socket.join(conversation);
+
+ 
+    
+
+      socket.emit('message', {
+        user,
+        text: `${user.firstName}, welcome to the chat `,
+      });
+      socket.broadcast
+        .to(conversation)
+        .emit('message', { user, text: `${user.firstName} has joined the chat!` });
+ 
+  });
+
+  socket.on("newnotif", async (data:any) => {
+      console.log(data,"/////////");
+      
+      const resData = data.notification.email
+      let arrF = onlineUser.find((user) =>{return  user.userId == resData});
+      console.log(arrF);
+      
+      if(arrF)
+      arrF.userSocket.map((x:any)=>{
+          io.to(x).emit("getNotfi",{data:data.notification});
+      })
+    
+
+  });
+  
+  socket.on('sendMessage', (message: any, user: any, discussionId: any, callback: any) => {
+    console.log("message.discussionId",message.discussionId);
+    
+    io.to(message.discussionId).emit('message', { user, text: message }); 
+
+  });
+
+});
 
 server.listen(port, () => {
   console.info(`--- 🌟  Started (${env}) --- http://localhost:${port}`);
